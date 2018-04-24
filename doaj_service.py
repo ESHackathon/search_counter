b29dc64a1568434f97a9673f46556a8d4addafcd
Juan Vasquez <ji.vasquez0@gmail.com>
2018-04-24 09:27:09 -0300

    Added DOAJ database

---
+++ doaj_service.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+
+def search_count(query):
+    # encoded_query = urllib.parse.quote_plus(query)
+    
+    url = """https://doaj.org/query/journal,article/_search?source={"query":{"query_string":{"query":"%s","default_operator":"AND"}},"from":0,"size":10,"facets":{"_type":{"terms":{"field":"_type","size":102,"order":"reverse_term"}},"index.classification.exact":{"terms":{"field":"index.classification.exact","size":110,"order":"count"}},"index.has_seal.exact":{"terms":{"field":"index.has_seal.exact","size":110,"order":"count"}},"index.license.exact":{"terms":{"field":"index.license.exact","size":110,"order":"count"}},"index.publisher.exact":{"terms":{"field":"index.publisher.exact","size":110,"order":"count"}},"index.language.exact":{"terms":{"field":"index.language.exact","size":110,"order":"count"}}}}""" % query
+    response = requests.get(url)
+    print response.content
+    return int(json.loads(response.content).get('hits', {}).get('total'))
--- search_counter_service.py
+++ search_counter_service.py
@@ -1,9 +1,11 @@
 
 from pubmed_service import search_count as pubmed_search_count
 from crossref_service import search_count as crossref_search_count
+from doaj_service import search_count as doaj_search_count
 
 def get_search_count(query):
 	return {
 		"pubmed": pubmed_search_count(query),
-		"crossref": crossref_search_count(query) 
+		"crossref": crossref_search_count(query),
+		"doaj": doaj_search_count(query) 
 	}
