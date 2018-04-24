c90cabdfc1d4a9ae7296fdc56fbc0d956c814993
Juan Vasquez <ji.vasquez0@gmail.com>
2018-04-24 12:44:58 -0300

    Quotes fixed

--- server.py
+++ server.py
@@ -29,5 +29,14 @@ def search_count_post():
 
     return json.dumps(get_search_count(query))
 
+
+
+@app.route("/upload-ris", methods=['POST'])
+def ris_test():
+	return "OK"
+
+
+
+
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", threaded=True)
