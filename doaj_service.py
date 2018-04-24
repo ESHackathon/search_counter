2d1cbaacba1cdc63e30c61a1ec33900be2f8f943
Daniel Pérez Rada <dperezrada@gmail.com>
2018-04-24 16:23:29 +0200

    Removing print

--- doaj_service.py
+++ doaj_service.py
@@ -8,5 +8,4 @@ def search_count(query):
     
     url = """https://doaj.org/query/journal,article/_search?source={"query":{"query_string":{"query":"%s","default_operator":"AND"}},"from":0,"size":10,"facets":{"_type":{"terms":{"field":"_type","size":102,"order":"reverse_term"}},"index.classification.exact":{"terms":{"field":"index.classification.exact","size":110,"order":"count"}},"index.has_seal.exact":{"terms":{"field":"index.has_seal.exact","size":110,"order":"count"}},"index.license.exact":{"terms":{"field":"index.license.exact","size":110,"order":"count"}},"index.publisher.exact":{"terms":{"field":"index.publisher.exact","size":110,"order":"count"}},"index.language.exact":{"terms":{"field":"index.language.exact","size":110,"order":"count"}}}}""" % query
     response = requests.get(url)
-    print response.content
     return int(json.loads(response.content).get('hits', {}).get('total'))
