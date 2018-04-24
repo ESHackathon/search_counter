5c1caa62189a2a9db1b224c5f03d801df7030c6e
Daniel Pérez Rada <dperezrada@gmail.com>
2018-04-24 09:48:56 +0200

    ADD: Pubmed search counter

---
+++ pubmed.py
@@ -0,0 +1,18 @@
+import json
+import urllib.parse
+
+import requests
+
+def search_count(query):
+    encoded_query = urllib.parse.quote_plus(query)
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=0&term=%s" % encoded_query
+    response = requests.get(url)
+    return json.loads(response.content)['esearchresult']['count']
+
+def main():
+    term = "Forest*"
+    total_count = search_count(term)
+    print("%s: %s" % (term, total_count))
+
+if __name__ == '__main__':
+    main()
