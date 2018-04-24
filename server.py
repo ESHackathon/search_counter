b66de4ad657d27d1b3e1d73e90bbff7be86b303c
Daniel Pérez Rada <dperezrada@gmail.com>
2018-04-24 13:58:29 +0200

    FIX: encoding problem

--- server.py
+++ server.py
@@ -4,20 +4,29 @@
 import json
 
 from flask import Flask, request
+from flask_cors import CORS
 
 from search_counter_service import get_search_count
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+def to_unicode(text):
+    if type(text) == unicode:
+        return text
+    return text.decode("utf-8")
+
 
 @app.route("/search-count/<query>", methods=['GET'])
 def search_count(query):
-    return json.dumps(get_search_count(query))
+    return json.dumps(get_search_count(to_unicode(query)))
 
 @app.route("/search-count", methods=['POST'])
 def search_count_post():
-    query = request.get_data()
+    query = to_unicode(request.get_data())
+
     return json.dumps(get_search_count(query))
 
 if __name__ == "__main__":
