392e339515d02c1928f13b1fed4c76c124377f06
Juan Vasquez <ji.vasquez0@gmail.com>
2018-04-24 05:55:39 -0300

    Added auto reload

--- server.py
+++ server.py
@@ -3,14 +3,19 @@
 
 import json
 
+from flask import Flask
+
 from search_counter_service import get_search_count
 
-from flask import Flask
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route("/search-count", methods=['GET'])
 def search_count():
     return json.dumps(get_search_count("colon AND cancer"))
 
 
-app.run()
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0", threaded=True)
